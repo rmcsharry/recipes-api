0d2b6a553ec22090d0411d62b46ffee4cb54287c
Richard McSharry <rmcsharry@gmail.com>
2023-11-27 21:20:09 +0100

    add basic tag feature and tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -5,24 +5,45 @@ from rest_framework import serializers
 
 from core.models import (
     Recipe,
+    Tag,
 )
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create and return a new tag."""
+        return Tag.objects.create(**validated_data)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
         fields = [
-            'id', 'title', 'time_minutes', 'price', 'link'
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags'
         ]
         read_only_fields = ['id']
 
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
-        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, _ = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag
+            )
+            recipe.tags.add(tag_obj)
 
         return recipe
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,13 +1,16 @@
 """
 Views for the recipe APIs
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+  Recipe, 
+  Tag
+)
 from recipe import serializers
 
 
@@ -34,3 +37,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.DestroyModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    """View for manage tags."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve tags for authenticated user."""
+        return self.queryset.filter(
+            user=self.request.user
+        ).order_by('-name')
