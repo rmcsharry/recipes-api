7104df589094f21f21d3b86a7d9d376a61df027b
Richard McSharry <rmcsharry@gmail.com>
2023-11-27 17:57:48 +0100

    add tests, model, migrations for Recipes

--- app/core/models.py
+++ app/core/models.py
@@ -4,37 +4,58 @@ Database models.
 import uuid
 import os
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
+
 class UserManager(BaseUserManager):
-  """Manager for users."""
+    """Manager for users."""
 
-  def create_user(self, email, password=None, **extra_fields):
-    """Creates, saves and returns a new user."""
-    if not email:
-      raise ValueError('Users must have an email address')
-    user = self.model(email=self.normalize_email(email), **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates, saves and returns a new user."""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-    return user
+        return user
 
-  def create_superuser(self, email, password):
-    """Creates and saves a new superuser."""
-    user = self.create_user(email, password)
-    user.is_staff = True
-    user.is_superuser = True
-    user.save(using=self._db)
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-    return user
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
-  email = models.EmailField(max_length=255, unique=True)
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    # tags = models.ManyToManyField('Tag')
+    # ingredients = models.ManyToManyField('Ingredient')
+    # image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self):
+        return self.title
