0089c2c0b44d99802124882134eef61bacb03732
Richard McSharry <rmcsharry@gmail.com>
2023-12-03 20:41:41 +0100

    make tests pass by adding update feature for ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -73,10 +73,16 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         """Update recipe."""
+        # not we are using None as default value for tags and ingredients
+        # so we can remove tags and ingredients using clear() method
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
